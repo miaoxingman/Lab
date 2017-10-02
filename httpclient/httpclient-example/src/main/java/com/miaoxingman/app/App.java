4a86cccdcb3805b7a8f3714c4dbe9c313970838b
miaoxingman <miaoxingman@gmail.com>
2017-10-02 08:36:14 +0800

    add httpclient library.

--- httpclient/httpclient-example/src/main/java/com/miaoxingman/app/App.java
+++ httpclient/httpclient-example/src/main/java/com/miaoxingman/app/App.java
@@ -1,4 +1,8 @@
 package com.miaoxingman.app;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -8,8 +12,22 @@ import org.apache.log4j.Logger;
 public class App 
 {
     private static Logger log = Logger.getLogger(App.class);
+
+    public static void httpclient_test() throws Exception {
+    		DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet("https://portal.sun.com/portal/dt");
+
+        HttpResponse response = httpclient.execute(httpget);
+        System.out.println("Login form get: " + response.getStatusLine());
+    }
+    
     public static void main( String[] args )
     {
         log.debug("Hello World!");
+        try {
+        		httpclient_test();
+        } catch (Exception e) {
+        	log.debug(e.getMessage());
+        }
     }
 }
