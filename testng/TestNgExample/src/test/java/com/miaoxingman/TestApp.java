76a2a0b463f008c684039c3008565b8a1969ede6
miaoxingman <miaoxingman@gmail.com>
2017-10-08 16:56:30 +0800

    TestNG – Configuration Annotations Example

--- testng/TestNgExample/src/test/java/com/miaoxingman/TestApp.java
+++ testng/TestNgExample/src/test/java/com/miaoxingman/TestApp.java
@@ -1,6 +1,13 @@
 package com.miaoxingman;
 
 import org.testng.Assert;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -8,12 +15,49 @@ import org.testng.annotations.Test;
  */
 public class TestApp
 {
-    @Test()
+    @BeforeGroups("shopping")
+    public void beforeGroups() {
+        System.out.println("@BeforeGroups");
+    }
+
+    @AfterGroups("shopping")
+    public void afterGroups() {
+        System.out.println("@AfterGroups");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("@BeforeClass");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("@AfterClass");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("@BeforeMethod");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("@AfterMethod");
+    }
+
+    @Test(groups = "shopping")
     public void testEmailGenerator() {
         App app = new App();
         String email = app.generateEmail();
 
+        System.out.println("@Test - testEmailGenerator");
+
         Assert.assertNotNull(email);
         Assert.assertEquals(email, "miaoxingman@gmail.com");
+    }
+
+    @Test
+    public void dummyTest() {
+        System.out.println("@Test - dummyTest");
     }
 }
