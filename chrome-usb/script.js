70026c64e590bba7a9267a667ba2ab8fe900cd01
miaoxingman <miaoxingman@gmail.com>
2017-11-06 21:49:47 +0800

    refactor with jQuery.

--- chrome-usb/script.js
+++ chrome-usb/script.js
@@ -1,24 +1,23 @@
-var device_selector = document.getElementById('device-selector');
-var add_device = document.getElementById('add-device');
-var device_info = document.getElementById('device-info');
+var device_selector = $('#device-selector');
+var add_device = $('#add-device');
+var device_info = $('#device-info');
 
 var devices = {};
 
 function appendToDeviceSelector(device) {
-  var el = document.createElement('option');
-  el.setAttribute('value', device.device);
-  el.textContent =
-      'Product 0x' + ('0000' + device.productId.toString(16)).slice(-4) +
-      ' Vendor 0x' + ('0000' + device.vendorId.toString(16)).slice(-4);
-  device_selector.add(el);
+  const productInfo = 'Product 0x' + ('0000' + device.productId.toString(16)).slice(-4) +
+    ' Vendor 0x' + ('0000' + device.vendorId.toString(16)).slice(-4);
+  device_selector.append($("<option></option>")
+    .attr("value",device.device)
+    .text(productInfo));
 };
 
 function appendDeviceInfo(name, value) {
   var el = document.createElement('b');
   el.textContent = name + ': '
-  device_info.appendChild(el);
-  device_info.appendChild(document.createTextNode(value));
-  device_info.appendChild(document.createElement('br'));
+  device_info[0].appendChild(el);
+  device_info[0].appendChild(document.createTextNode(value));
+  device_info[0].appendChild(document.createElement('br'));
 }
 
 function populateDeviceInfo(handle, callback) {
@@ -27,16 +26,16 @@ function populateDeviceInfo(handle, callback) {
       var el = document.createElement('em');
       el.textContent = 'Failed to read device configuration: ' +
           chrome.runtime.lastError.message;
-      device_info.appendChild(el);
+      device_info[0].appendChild(el);
     } else {
       var el = document.createElement('h2');
       el.textContent = 'Configuration ' + config.configurationValue;
-      device_info.appendChild(el);
+      device_info[0].appendChild(el);
 
       for (var iface of config.interfaces) {
         el = document.createElement('h3');
         el.textContent = 'Interface ' + iface.interfaceNumber;
-        device_info.appendChild(el);
+        device_info[0].appendChild(el);
 
         appendDeviceInfo('Alternate Setting', iface.alternateSetting);
         appendDeviceInfo('Inteface Class', iface.interfaceClass);
@@ -46,7 +45,7 @@ function populateDeviceInfo(handle, callback) {
         for (var endpoint of iface.endpoints) {
           el = document.createElement('h4');
           el.textContent = 'Endpoint ' + endpoint.address;
-          device_info.appendChild(el);
+          device_info[0].appendChild(el);
 
           appendDeviceInfo('Type', endpoint.type);
           appendDeviceInfo('Direction', endpoint.direction);
@@ -60,15 +59,15 @@ function populateDeviceInfo(handle, callback) {
 }
 
 function deviceSelectionChanged() {
-  device_info.innerHTML = "";
+  device_info[0].innerHTML = "";
 
-  var index = device_selector.selectedIndex;
+  var index = device_selector[0].selectedIndex;
   if (index == -1) {
     var el = document.createElement('em');
     el.textContent = 'No device selected.';
-    device_info.appendChild(el);
+    device_info[0].appendChild(el);
   } else {
-    var device = devices[device_selector.options.item(index).value];
+    var device = devices[device_selector[0].options.item(index).value];
 
     appendDeviceInfo(
         'Product ID',
@@ -82,7 +81,7 @@ function deviceSelectionChanged() {
         var el = document.createElement('em');
         el.textContent = 'Failed to open device: ' +
             chrome.runtime.lastError.message;
-        device_info.appendChild(el);
+        device_info[0].appendChild(el);
       } else {
         populateDeviceInfo(handle, function () {
           chrome.usb.closeDevice(handle);
@@ -115,9 +114,9 @@ if (chrome.usb.onDeviceAdded) {
 if (chrome.usb.onDeviceRemoved) {
   chrome.usb.onDeviceRemoved.addListener(function (device) {
     delete devices[device.device];
-    for (var i = 0; i < device_selector.length; ++i) {
-      if (device_selector.options.item(i).value == device.device) {
-        device_selector.remove(i);
+    for (var i = 0; i < device_selector[0].length; ++i) {
+      if (device_selector[0].options.item(i).value == device.device) {
+        device_selector[0].remove(i);
         deviceSelectionChanged();
         break;
       }
@@ -125,7 +124,7 @@ if (chrome.usb.onDeviceRemoved) {
   });
 }
 
-add_device.addEventListener('click', function() {
+add_device.click(function() {
   chrome.usb.getUserSelectedDevices({
     'multiple': false
   }, function(selected_devices) {
@@ -138,20 +137,20 @@ add_device.addEventListener('click', function() {
     for (var device of selected_devices) {
       var deviceInfo = { 'device': device, 'index': undefined };
       if (device.device in devices) {
-        for (var i = 0; i < device_selector.length; ++i) {
-          if (device_selector.options.item(i).value == device.device) {
-            device_selector.selectedIndex = i;
+        for (var i = 0; i < device_selector[0].length; ++i) {
+          if (device_selector[0].options.item(i).value == device.device) {
+            device_selector[0].selectedIndex = i;
             break;
           }
         }
       } else {
         devices[device.device] = device;
         appendToDeviceSelector(device);
-        device_selector.selectedIndex = device_selector.options.length - 1;
+        device_selector[0].selectedIndex = device_selector[0].options.length - 1;
       }
       deviceSelectionChanged();
     }
   });
 });
 
-device_selector.addEventListener('input', deviceSelectionChanged);
+device_selector.change(deviceSelectionChanged);
